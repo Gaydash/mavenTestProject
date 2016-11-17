84d3ffc7a8303ec12951bba46377ff88c1262371
Olga <o.gaydash@slice.com>
2016-11-17 17:31:36 +0200

    first commit

---
+++ src/main/java/MavenTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Olga on 17.11.2016.
+ */
+public class MavenTest {
+    public static void main(String[] args) {
+        System.out.println("Start maven project in jenkins");
+    }
+}
